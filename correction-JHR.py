ee37e535d037a52b9084ed37f74a958684e7e118
jhroy <jh@jhroy.ca>
2017-03-20 17:04:35 -0400

    Correction complétée

---
+++ correction-JHR.py
@@ -0,0 +1,62 @@
+### MES COMMENTAIRES ET CORRECTIONS SONT MARQUÉS PAR TROIS DIÈSES
+### Excellent script!
+### Rien à redire! :)
+
+# coding utf-8
+
+import csv
+import requests
+from bs4 import BeautifulSoup
+
+fichier = "especes-menacees-qc.csv"
+
+#Parce que je ne parvenais pas à isoler le segment dans la page [http://www3.mffp.gouv.qc.ca/faune/especes/menacees/liste.asp] pour mener à la fiche des espèces menacées recensées, j'ai décidé de "patcher" moi-même l'url avec un range entre 0 et 88. 
+### Bonne solution!
+
+#Après vérifications, le nombre d'espèces menacées recensées ne dépasse pas 88. Et quelques nombres, comme le 12 par exemple, ne sont associés à aucune espèce.
+especes = list(range(1,89))
+
+entetes = {
+    "User-Agent": "Guillaume Lepage - Requête pour un cours de journalisme à l'UQAM",
+    "From": "guillaume.p.lepage@gmail.com"
+}
+
+#J'ai laissé les traces de ma première tentative infructueuse de moissonnage. J'ai réussi à contourner mon problème avec la var "especes" susmentionnée qui renvoie à mon "range" pour compléter directement l'hyperlien initial.
+
+# url = "http://www3.mffp.gouv.qc.ca/faune/especes/menacees/liste.asp"
+# contenu = requests.get(url, headers=entetes)
+# page = BeautifulSoup(contenu.text, "html.parser")
+# ligne = page.find_all("td")
+# for td in ligne:
+#     fin = td.find_all(href=True)
+#     print(fin.a["href"])
+
+#Donc, une fois arrivé à cette étape, je ne savais pas comment je pouvais isoler le bout d'url, unique à chaque espèce, dont j'allais me servir pour compléter mon hyperlien initial.
+    
+for espece in especes:
+    url = "http://www3.mffp.gouv.qc.ca/faune/especes/menacees/fiche.asp?noEsp={}".format(espece)
+    contenu = requests.get(url, headers=entetes)
+    #Avec les deux étapes ci-dessous, je vérifie si j'ai une réponse positive de ma requête auprès du site du MFFP [statut 200], donc si les manipulations de ma première "loop" fonctionne.
+    # if contenu.status_code==200:
+    # print(contenu)
+    page2 = BeautifulSoup(contenu.text, "html.parser")
+    #Je crée ma variable finale dans laquelle je vais mettre les infos au fur et à mesure que je vais moissonner les profils des espèces menacées.
+    info = []
+    info.append(url)
+    #Pour trouver le nom de chacune des espèces menacées sur leur profil respectif du site du MFFP.
+    for nom in page2.find_all("p", class_="titre"):
+        # print("%"*50)
+        # print(url)
+        # print(nom.text)
+        info.append(nom.text.strip())
+        for rang in page2.find_all("td", class_="rang"):
+            if rang.text.strip()[0:4] != "Rang":
+                if rang.text.strip()[0:6] != "Statut":
+                    info.append((rang.text.strip()))
+                    print(info)
+        # print(page2.find("table", id="AutoNumber2").find("p").text.strip())
+
+        #Et on remplit les lignes et les colonnes de notre fichier csv avec les données récoltées dans nos "loops" précédentes et assignées à la var «info».
+        ecrire = open(fichier,"a")
+        lucBlanchette = csv.writer(ecrire)
+        lucBlanchette.writerow(info)
